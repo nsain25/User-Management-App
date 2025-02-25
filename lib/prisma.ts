cffc37dca11d8a32368409d8a660400881ab1d97
Nandini <nandinisain@yahoo.in>
2025-02-26 01:48:24 +0530

    Fix ESLint issue: Replace var with let

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 declare global {
   // Allow global `var` here to work with Next.js's hot reloading
-  var prisma: PrismaClient | undefined;
+  let prisma: PrismaClient | undefined;
 }
 
 const client = global.prisma || new PrismaClient();
