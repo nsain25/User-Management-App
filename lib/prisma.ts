bf6038d0dac4d35ff93e7f6f9b2ed0112ec7e698
Nandini <nandinisain@yahoo.in>
2025-02-26 11:11:42 +0530

    Fix Prisma redeclaration issue by using globalThis

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,10 @@
 // lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // Allow global `let` here to work with Next.js's hot reloading
-  let prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
 
-const prisma = global.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
